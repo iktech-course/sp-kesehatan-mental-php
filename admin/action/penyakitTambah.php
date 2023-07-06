73a8d14c50b01be201ef68125e272d64bf137b68
olifnuralfath026 <holiparoliah12@gmail.com>
2023-07-06 14:29:56 +0700

    mengubah penyakit tambah

--- admin/action/penyakitTambah.php
+++ admin/action/penyakitTambah.php
@@ -6,7 +6,7 @@ $solusi = $_POST['solusi'];
 
 include("../../db/db_conn.php");
 
-$tambah = mysqli_query($conn, "INSERT INTO tbl_penyakit(id_penyakit, name, solusi, langkah_pencegahan) VALUES('$id','$nama','$solusi','$langkah')");
+$tambah = mysqli_query($conn, "INSERT INTO tbl_penyakit(id_penyakit, name, solusi) VALUES('$id','$nama','$solusi')");
 
 
 if($tambah){
